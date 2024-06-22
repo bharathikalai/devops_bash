653437979ae0fd71524b1cf6068eda5da398d3ef
bharathibk <bharathikalai.bk@gmail.com>
2024-06-22 22:05:07 +0530

    new

---
+++ linux_bash/day1.sh
@@ -0,0 +1,17 @@
+File system : linux
+
+/ root file >>> 
+The top-level directory in the file system hierarchy.
+All other directories and files stem from the root directory.
+Contains essential system files, configuration files, and directories.
+
+
+
+bin >> Contains essential binary executable files (programs) used by the system and users.
+Common system utilities such as ls, cp, mkdir, etc., reside here.
+
+
+(GRand Unified Bootloader)
+
+
+
---
+++ shell_basic_to_adavnce/day1.sh
@@ -0,0 +1,35 @@
+# for x in {1..99..2};
+# do
+#   echo $x
+# done
+
+
+
+# for x in {1..99};
+# do 
+#   echo $x
+# done
+
+
+# for z in {1..100..3};
+# do 
+# echo $z
+# done
+
+
+
+
+DIR="/home/bharathibk/github/devops_bash/shell_basic_to_adavnce/day1.sh"
+
+
+if [ -f "$DIR" ]; then
+
+
+
+echo "the dir $DIR exists."
+                                        else
+echo "the dir $DIR not exists"
+                                         fi
+
+
+file="/home/bharathibk/github/devops_bash/shell_basic_to_adavnce/"
